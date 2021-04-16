dea989c63acc6c7d3c16dafc63b23af1a14b2506
nylahtay <nylahtayrogers@gmail.com>
2021-04-16 17:55:45 -0500

    #MAIN - added a working example to the main activity

--- app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
+++ app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
@@ -1,19 +1,113 @@
 package com.example.locationsensorsgroupproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    //Initialize variables
+    Button btLocation;
+    TextView textView1, textView2, textView3, textView4, textView5;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Assign the variables
+        btLocation = findViewById(R.id.button_location);
+        textView1 = findViewById(R.id.textView);
+
+        //Initialize fusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        btLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                    getLocation();
+            }
+        });
+
+
+    }
+
+    //Had to add the @supressLint for this since we are doing the permission check above
+    //@SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        //Check to see if you have permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // calling ActivityCompat#requestPermissions here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+
+            String errorString = "Permission Denied";
+            Toast.makeText(MainActivity.this, "errorString", Toast.LENGTH_LONG);
+            textView1.setText(errorString);
+
+            //this will ask the user for permission
+            // to handle the case where the user grants the permission.
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //Initialize location
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+                        //initialze the geoCoder
+                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+
+                        //Initialize address list
+                        List<Address> addresses = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1
+                        );
+
+                        //Set latitude on TextView
+                        textView1.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Country Name:</b><br></font>" + addresses.get(0).getCountryName()
+                                        + " Address:" + addresses.get(0).getAddressLine(0) + " " + addresses.get(0).getAddressLine(1)
+                                        + " Longitude:" + addresses.get(0).getLongitude() + " Latitude:" + addresses.get(0).getLatitude()
+
+                        ));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                else {
+                    textView1.setText("Location is set to NULL on this device");
+                }
+            }
+        });
     }
 }
