1e649039a3da2952a0d2194fbe42e95596cb901b
CarlCaldwell <cc0217891@otc.edu>
2021-04-16 19:54:44 -0500

    Added distance between functionality

--- app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
+++ app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
@@ -37,11 +37,46 @@ public class MainActivity extends AppCompatActivity {
         }
         try{
             Address address = addresses.get(0);
-            return address.getLongitude() + " "+ address.getLatitude();
+
+
+            // output the gps as lat/lon
+            //return "Lat/Lon: "+address.getLatitude() +" "+address.getLongitude();
+
+            // output distance
+            // create a second location
+            // this is the white house in washington dc
+            double lat2 = 38.8976763;
+            double lon2 = -77.0365298;
+            return "Distance to the White House is "+distance(address.getLongitude(),  address.getLatitude(), lon2, lat2)+" miles";
+
         }catch(Exception e){
             return "Could not locate";
         }
 
+    }
+
+    private double distance(double longitude1,double latitude1, double longitude2,double latitude2){
+        double lon1 = Math.toRadians(longitude1);
+        double lon2 = Math.toRadians(longitude2);
+        double  lat1 = Math.toRadians(latitude1);
+        double  lat2 = Math.toRadians(latitude2);
+
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        double r = 6371;
+
+        // calculate the result
+        double distance = c * r;
+
+        return Math.floor(distance*.621371);
     }
 
 }
