fd54255ed310fe3d302220bf92afa2d1344e046f
CarlCaldwell <cc0217891@otc.edu>
2021-04-19 10:28:54 -0500

    blah

--- app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
+++ app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.locationsensorsgroupproject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import androidx.annotation.NonNull;
+import android.location.Location;
+import android.text.Html;
 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 =======
 public class MainActivity extends AppCompatActivity {
@@ -18,13 +27,24 @@ public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient fusedLocationProviderClient;
     double lat1, lon1, lat2, lon2;
 >>>>>>> Stashed changes
+=======
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+>>>>>>> Stashed changes
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btLocation;
+    TextView textView1, textView2, textView3, textView4, textView5;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 =======
 
@@ -44,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
         getLocation();
 
 
+>>>>>>> Stashed changes
+=======
+
+        btLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                getLocation();
+            }
+        });
 >>>>>>> Stashed changes
     }
 
@@ -195,6 +225,70 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 >>>>>>> Stashed changes
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////////////////
+
+
+    //Had to add the @supressLint for this since we are doing the permission check above
+    //@SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        //Check to see if you have permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // calling ActivityCompat#requestPermissions here to request the missing permissions
+
+            String errorString = "Permission Denied";
+            Toast.makeText(MainActivity.this, errorString, Toast.LENGTH_LONG).show();
+            textView1.setText(errorString);
+
+            //this will ask the user for permission
+            // to handle the case where the user grants the permission.
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+
+            return;
+        }
+
+        //This will check the phones location
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //Initialize location
+                Location location = task.getResult();
+                if (location != null) {
+                    //Location isn't null
+                    try {
+                        //initialize the geoCoder
+                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+
+                        //Initialize address list
+                        List<Address> addresses = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1
+                        );
+
+                        //Set latitude on TextView
+                        textView1.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Country Name:</b><br></font>" + addresses.get(0).getCountryName()
+                                        + " Address:" + addresses.get(0).getAddressLine(0) + " " + addresses.get(0).getAddressLine(1)
+                                        + " Longitude:" + addresses.get(0).getLongitude() + " Latitude:" + addresses.get(0).getLatitude()
+
+                        ));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                else {
+                    //location returned null from task.getResult()
+                    String errorString = "Location is set to NULL on this device";
+                    Toast.makeText(MainActivity.this, errorString, Toast.LENGTH_LONG).show();
+                    textView1.setText(errorString);
+                }
+            }
+        });
     }
 
 }
