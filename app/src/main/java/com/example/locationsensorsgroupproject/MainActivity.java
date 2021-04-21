cf091e4c1d9a20c4b1c4f0d233c390359bc3290b
CarlCaldwell <cc0217891@otc.edu>
2021-04-20 23:10:00 -0500

    Optimized Imports

--- app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
+++ app/src/main/java/com/example/locationsensorsgroupproject/MainActivity.java
@@ -1,11 +1,6 @@
 package com.example.locationsensorsgroupproject;
 
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.ShareActionProvider;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,25 +8,19 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-
-import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
